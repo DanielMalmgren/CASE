fa5d86aa1b6ea0edc2eca3ec3be782194d03b497
DanielMalmgren <daniel@kolefors.se>
2020-11-12 10:08:55 +0000

    Fix crash when wieving question in non default language

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -39,7 +39,7 @@
 
         <div class="card">
             <div class="card-body">
-                {{$question->translateOrDefault(App::getLocale())->text}}
+                {{$question->translation()->text}}
             </div>
         </div>
 
