03f606517f860a44b17fad7fda4040f8e3881aee
DanielMalmgren <daniel@kolefors.se>
2021-07-02 05:57:49 +0000

    Fix spanish translation of info page

--- resources/lang/es_ES/messages.php
+++ resources/lang/es_ES/messages.php
@@ -1,11 +1,16 @@
-<?php
-
+<?php 
 return [
-    'gdprinfo' => 'Evikomp es un proyecto ESF.<br>
-                   <a href="https://www.linkoping.se/utforarwebben/vard-stod-och-omsorg/forskning-och-utveckling/pagaende-projekt/evikomp/">
-                   Para obtener más información sobre el proyecto, haga clic aquí.</a><br><br>
-                   Esta plataforma web forma parte del proyecto. Para que usted lo use, su información personal necesita ser almacenada. <br> <br>
-                   Los únicos que, además de usted, pueden ver sus datos personales son los operadores del sistema, su gerente y el coordinador del lugar de trabajo. <br>
-                   Los datos estadísticos del proyecto se utilizarán para investigación y desarrollo de forma anónima. <br> <br>
-                   Al hacer clic en "Continuar" a continuación, usted reconoce que se le ha informado sobre el almacenamiento descrito anteriormente.',
+  'infotext' => 'Esta plataforma se ha desarrollado en cooperación entre cinco actores en
+                cinco países europeos.<br>
+                Tenemos desafíos en común como consecuencia de una población cada vez más envejecida en Europa. La demencia aumenta y queremos ofrecer el mejor cuidado posible.<br>
+                Para eso hace falta personal bien formado y capacitado. Esta plataforma ofrece una posibilidad para el personal nuevo e inexperto en el sector de la asistencia sanitaria y social de desarrollar sus conocimientos sobre la demencia y el cuidado de las personas que padecen estas enfermedades.<br><br>
+                Case está financiado por la Unión Europea a través de Erasmus+.',
+    'infotext_long' => 'El proyecto está integrado por las siguientes partes:
+                    <ul>
+                    <li>El proyecto fue iniciado por la Unidad de Investigación y Desarrollo de la Administración Municipal de Linköping. La unidad es una colaboración entre los municipios de la provincia, la Diputación Provincial de Östergötland y la Universidad de Linköping. I&amp;D tiene relaciones estrechas con una red de proveedores de servicios de salud, tanto municipales como privados, con los servicios sociales y el trabajo social en la región, igual que con los centros de desarrollo nacionales en Suecia.</li>
+                    <li>Dundee and Angus College, uno de los mayores coordinadores de educación en Escocia, con una larga experiencia en ofrecer formación en asistencia sanitaria y social, desde el nivel introductorio hasta nivel avanzado. Asimismo, trabaja en colaboración estrecha con empresas y empleadores, tanto en el sector privado como el público. Este centro educativo superior también tiene muchos años de experiencia conduciendo o participando en colaboraciones dentro de la UE, a través de Erasmus y programas relacionados.</li>
+                    <li>Centro de Formación Somorrostro, un centro integrado de enseñanza y prácticas ubicado en el País Vasco en España. Como el segundo coordinador de formación más grande en su región, el centro tiene muchos años de experiencia de trabajo en el sector de la salud y de participar en asociaciones internacionales a través de Erasmus.</li>
+                    <li>Zemgale Planning Region (ZPR), un organismo público en Letonia responsable de la planificación regional y la coordinación de servicios como el transporte, la formación, los servicios sociales, la protección medioambiental y el apoyo a las empresas. Su organización de desarrollo comprende 22 municipios.</li>
+                    <li>Teacher Training Centre Ilfov (TTC Ilfov), un centro independiente de prácticas y recursos en Romanía, especializado en el aprendizaje profesional y continuo en el lugar de trabajo.</li>
+                    </ul>',
 ];
